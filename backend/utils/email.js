91a15a696acd1d2921591afca5cbbca435d69625
lukebobjoseph <lukebobjoseph@gmail.com>
2026-04-07 20:46:59 -0400

    fix

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,13 +3,16 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const sendVerificationEmail = async (email, token) => {
