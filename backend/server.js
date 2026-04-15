4956fc74161ba836508bd9f63bf0f03ecdea7d4c
Jershw43 <95701884+jershw43@users.noreply.github.com>
2026-04-15 02:17:17 -0400

    Updated AI placeholder

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./db');
+const connectDB = require('./config/db');
 
 const app = express();
 
